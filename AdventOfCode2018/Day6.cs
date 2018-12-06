1d94984ad63c045e652af2463a6bbfde9584ae62
Adam Hoka <adam.hoka@gmail.com>
2018-12-06 06:42:42 +0100

    Add parser for day 6

---
+++ AdventOfCode2018/Day6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2018
+{
+    public class Day6
+    {
+        public class Coordinate
+        {
+            public Coordinate(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; }
+            public int Y { get; }
+        }
+
+        private static Regex pattern = new Regex("(?<x>). (?<y>)");
+        public IEnumerable<Coordinate> Input => File.ReadAllLines("input6.txt")
+            .Select(x => pattern.Match(x).Groups)
+            .Select(x => new Coordinate(int.Parse(x['x'].Value), int.Parse(x['y'].Value)));
+
+        public static int SolveFirst()
+        {
+
+        }
+    }
+}
--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -6,13 +6,7 @@ namespace AdventOfCode2018
     {
         static void Main(string[] args)
         {
-            //var solution1a = Day1.SolveFirst();
-            //Console.WriteLine($"Day 1 First Solution: {solution1a}");
-
-            //var solution1b = Day1.SolveSecond();
-            //Console.WriteLine($"Day 1 First Solution: {solution1b}");
-
-            var result = Day5.SolveSecond();
+            var result = Day6.SolveFirst();
             Console.WriteLine(result);
         }
     }
